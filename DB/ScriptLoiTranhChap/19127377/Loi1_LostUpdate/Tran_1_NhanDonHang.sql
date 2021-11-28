58dfbdfd7d76a99ff6b5205d113ebf0f612e3f78
nhkduy201 <nhkduy201@gmail.com>
2021-11-28 23:43:27 +0700

    script loi 1

---
+++ DB/ScriptLoiTranhChap/19127377/Loi1_LostUpdate/Tran_1_NhanDonHang.sql
@@ -0,0 +1,6 @@
+--chua fix loi
+exec NhanDonHang1 @MaTX = 'TX0001', @MaDH = 'DH0001'
+--da fix loi
+--exec NhanDonHangFix1 @MaTX = 'TX0001', @MaDH = 'DH0001'
+--select * from DONHANG
+--update DONHANG set MaTaiXe = null where MaDH = 'DH0001'
---
+++ DB/ScriptLoiTranhChap/19127377/Loi1_LostUpdate/Tran_2_NhanDonHang.sql
@@ -0,0 +1,6 @@
+--chua fix loi
+exec NhanDonHang2 @MaTX = 'TX0002', @MaDH = 'DH0001'
+--da fix loi
+--exec NhanDonHangFix2 @MaTX = 'TX0002', @MaDH = 'DH0001'
+--select * from DONHANG
+--update DONHANG set MaTaiXe = null where MaDH = 'DH0001'
