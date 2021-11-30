4f615f75bc00b3923c7ca4218da66dacaadb5ea2
Nguyễn Thị Như Phương <92770267+nhuphuongs@users.noreply.github.com>
2021-11-30 15:16:54 +0700

    Update Tran_2_DirtyRead.sql

--- DB/ScriptLoiTranhChap/19127521/DirtyRead/Tran_2_DirtyRead.sql
+++ DB/ScriptLoiTranhChap/19127521/DirtyRead/Tran_2_DirtyRead.sql
@@ -1,1 +1,3 @@
-﻿declare @Gia intexec sp_XemSanPham 'SP0001', @Gia outprint N'Giá của sản phẩm là: ' + cast(@Gia as varchar(20))
+declare @Gia int
+exec sp_XemSanPham 'SP0001', @Gia out
+print N'Giá của sản phẩm là: ' + cast(@Gia as varchar(20))
